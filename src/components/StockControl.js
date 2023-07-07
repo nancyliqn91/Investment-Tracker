8ffe26eda1e6a84d02119e3f28bac6cd676779cc
nancyliqn91 <nancyliqn91@gmail.com>
2023-07-07 16:05:47 -0700

    add StockControl

--- src/components/StockControl.js
+++ src/components/StockControl.js
@@ -1,17 +1,116 @@
 import React from 'react';
 import NewTickerForm from './NewTicketForm';
-import StockList from './StockList';
+import TickerList from './TickerList';
+import TickerDetail from './TickerDetail';
+import EditTickerForm from './EditTickerForm';
+import Stocks from './Stocks';
 
 class StockControl extends React.Component {
 
   constructor(props) {
     super(props);
-    this.state = {};
+    this.state = {
+      formVisibleOnPage:false,
+      mainTickerList: [], 
+      selectedTicker: null,
+      editing: false
+    };
   }
 
-  render(){
+  handleClick = () => {
+    if (this.state.selectedTicker != null) {
+      this.setState({
+        formVisibleOnPage: false,
+        selectedTicker: null,
+        editing: false
+      });
+    } else {
+      this.setState(prevState => ({
+        formVisibleOnPage: !prevState.formVisibleOnPage,
+      }));
+    }
+  }
+
+  handleAddingNewTickerToList = (newTicker) => {
+    const newMainTickerList = this.state.mainCoffeeList
+    .concat(newTicker);
+    this.setState({mainTickerList: newMainTickerList,
+                  formVisibleOnPage: false });
+  }
+
+  handleChangingSelectedTicker = (id) => {
+    const selectedTicker = this.state.mainTickerList
+    .filter(ticker => ticker.id === id)[0];
+    this.setState({selectedTicker: selectedTicker});
+  }
+
+  handleDeletingTicker = (id) => {
+    const newMainTickerList = this.state.mainTickerList
+    .filter(ticker => ticker.id !== id);
+    this.setState({
+      mainTickerList: newMainCoffeeList,
+      selectedTicker: null
+    });
+  }
+
+  handleEditClick = () => {
+    this.setState({editing: true});
+  }
+
+  handleEditingTickerInList = (tickerToEdit) => {
+    const editedMainTickerList = this.state.mainTickerList
+      .filter(ticker => ticker.id !== this.state.selectedTicker.id)
+      .concat(tickerToEdit);
+    this.setState({
+        mainTickerList: editedMainTickerList,
+        editing: false,
+        selectedTicker: null
+      });
+  }
+
+  handleBuyClick = () => {
+    const selectedTicker = this.state.selectedTicker;
+
+    if (selectedCoffee.quantity > 0) {
+    this.setState({
+      mainTickerList: this.state.mainTickerList
+      .filter(ticker => ticker.id !== selectedTicker.id)
+      .concat({...selectedTicker, quantity: selectedTicker.quantity - 1}), 
+      selectedTicker: null })
+    }
+  }
+
+    render(){
+    let currentlyVisibleState = null;
+    let buttonText = null; 
+
+    if (this.state.editing ) {      
+      currentlyVisibleState = <EditTickerForm ticker = {this.state.selectedTicker} 
+      onEditTicker = {this.handleEditingTickerInList}/>
+
+      buttonText = "Return to Ticker List";
+    } 
+    else if (this.state.selectedTicker != null) {
+      currentlyVisibleState = <TickerDetail ticker = {this.state.selectedTicker} 
+      onClickingDelete = {this.handleDeletingTicker} onClickingEdit = {this.handleEditClick}  onClickingBuy={this.handleBuyClick} />
+
+      buttonText = "Return to Ticker List";
+    } 
+    else if (this.state.formVisibleOnPage) {
+      currentlyVisibleState = <NewTickerForm onNewTickerCreation={this.handleAddingNewTickerToList} />
+      
+      buttonText = "Return to Ticker List";
+    } 
+    else {
+      currentlyVisibleState = <TickerList tickerList={this.state.mainTickerList} 
+      onTickerSelection={this.handleChangingSelectedTicker}/>;     
+      buttonText = "Add Ticker"; 
+    }
+
     return (
       <React.Fragment>
+        {currentlyVisibleState}
+        <button onClick={this.handleClick}>{buttonText}</button> 
       </React.Fragment>
     );
   }
