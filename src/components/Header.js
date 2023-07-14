bb570778a10fb12d47dfcb85c1fe6ecffeda8ad5
nancyliqn91 <nancyliqn91@gmail.com>
2023-07-13 23:11:34 -0700

    add router

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,10 +1,25 @@
 import React from "react";
+import { Link } from "react-router-dom";
 
 function Header(){
   return (
     <React.Fragment>
     <h1 class="header">Welcome to Investment Tracker!</h1>
     <h3 class="header">🌰 You are welcome to search and track your stocks and coins! 🌰</h3>
+    <ul>
+        <li>
+          <Link to="/">Home</Link>
+        </li>
+        <li>
+          <Link to="/sign-in">Sign In</Link>
+        </li>
+        <li>
+          <Link to="/stocks">Stocks</Link>
+        </li>
+        <li>
+          <Link to="/chatGPT">chatGPT</Link>
+        </li>
+      </ul>
     </React.Fragment>
   );
 }
