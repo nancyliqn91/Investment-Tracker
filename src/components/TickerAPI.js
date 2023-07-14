8b6649842fb3d11cd287aed3d998fe9fadf5debc
nancyliqn91 <nancyliqn91@gmail.com>
2023-07-14 15:50:38 -0700

    WIP

--- src/components/TickerAPI.js
+++ src/components/TickerAPI.js
@@ -1,40 +1,50 @@
 import axios from "axios";
-import PropTypes from "prop-types";
-import React from "react";
+import React, { useEffect, useState } from "react";
 
 const apiKey = process.env.REACT_APP_OPENAI_API_KEY;
 const baseURL = "https://api.polygon.io/v2/aggs/ticker";
 
-const generateTicker = async (props) => {
-  try {
-    const { ticker } = props; 
+const TickerAPI = (props) => {
+  const [data, setData] = useState(null);
+  const [error, setError] = useState(null);
 
-    const response = await axios.post(
-      `${baseURL}/${props.name}/range/${props.multiplier}/${props.timespan}/${props.from}/${props.to}?adjusted=true&sort=asc&limit=10&apiKey=${apiKey}`);
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const response = await axios.post(
+          `${baseURL}/${props.name}/range/${props.multiplier}/${props.timespan}/${props.from}/${props.to}?adjusted=true&sort=asc&limit=10&apiKey=${apiKey}`
+        );
+        setData(response.data.results);
+      } catch (error) {
+        console.error(error);
+        setError("Error generating ticker");
+      }
+    };
 
-    return (
-        <React.Fragment>
-          <h1>Ticker: {props.name} </h1>
-          <ul>
-            {response.data.results.map((stock, index) =>
-              <li key={index}>
-                {/* <h3>Ticker:{stock.ticker}</h3> */}
-                <p>open price:{stock.o}</p>
-                <p>close price:{stock.c}</p>
-              </li>
-            )}
-          </ul>
-        </React.Fragment>
-      );
-  } 
-  catch (error) {
-    console.error(error);
-    throw new Error("Error generating ticker");
+    fetchData();
+  }, [props]);
+
+  if (error) {
+    return <p>{error}</p>;
   }
-}
 
-generateTicker.propTypes = {
-  ticker: PropTypes.object
+  if (!data) {
+    return null; // Render loading state or return a placeholder
+  }
+
+  return (
+    <React.Fragment>
+      <h1>Ticker: {props.name} </h1>
+      <ul>
+        {data.map((stock, index) => (
+          <li key={index}>
+            <p>open price: {stock.o}</p>
+            <p>close price: {stock.c}</p>
+          </li>
+        ))}
+      </ul>
+    </React.Fragment>
+  );
 };
 
-export default generateTicker
+export default TickerAPI;
