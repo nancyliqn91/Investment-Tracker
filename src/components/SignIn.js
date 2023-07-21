8d408ae52f2d78ad35301a416bd3773f875d9138
nancyliqn91 <nancyliqn91@gmail.com>
2023-07-21 11:25:59 -0700

    update sign-in

--- src/components/SignIn.js
+++ src/components/SignIn.js
@@ -1,99 +1,63 @@
 import React, { useState } from "react";
 import { auth } from "./../firebase.js";
-import { createUserWithEmailAndPassword, signInWithEmailAndPassword, signOut } from "firebase/auth";
+import { signInWithEmailAndPassword } from "firebase/auth";
+import { useNavigate } from 'react-router-dom';
 
-import Button from 'react-bootstrap/Button';
-import Card from 'react-bootstrap/Card';
-import Form from 'react-bootstrap/Form';
+import Form from "react-bootstrap/Form";
+import Button from "react-bootstrap/Button";
+import Alert from "react-bootstrap/Alert";
+import Container from "react-bootstrap/Container";
+import Row from "react-bootstrap/Row";
+import Col from "react-bootstrap/Col";
 
-function SignIn(){  
-  const [signUpSuccess, setSignUpSuccess] = useState(null);
+function SignIn(){   
   const [signInSuccess, setSignInSuccess] = useState(null);
-  const [signOutSuccess, setSignOutSuccess] = useState(null);
+  const navigate = useNavigate();
 
-  function doSignUp(event) {
-    event.preventDefault();
-    const email = event.target.email.value;
-    const password = event.target.password.value;
-    createUserWithEmailAndPassword(auth, email, password)
-      .then((userCredential) => {      
-        setSignUpSuccess(`You've successfully signed up, ${userCredential.user.email}!`)
-      })
-      .catch((error) => {
-        setSignUpSuccess(`There was an error signing up: ${error.message}!`)
-      });
-  }
-  
   function doSignIn(event) {
     event.preventDefault();
-    const email = event.target.signinEmail.value;
+    const email = event.target.email.value;
     const password = event.target.signinPassword.value;
     signInWithEmailAndPassword(auth, email, password)
       .then((userCredential) => {
-        setSignInSuccess(`You've successfully signed in as ${userCredential.user.email}!`)
+        setSignInSuccess(`You've successfully signed in as ${userCredential.user.email}!`);
+        navigate('/');
       })
       .catch((error) => {
         setSignInSuccess(`There was an error signing in: ${error.message}!`)
       });
   }
 
-  function doSignOut() {
-    signOut(auth)
-      .then(function() {
-        setSignOutSuccess("You have successfully signed out!");
-      }).catch(function(error) {
-        setSignOutSuccess(`There was an error signing out: ${error.message}!`);
-      });
-  }
-
   return (
     <React.Fragment>
+    <Container className="my-auto">
+      <Row>
+        <Col md={{ span: 6, offset: 3 }}>
+          <h1 className="mb-3">Sign in to Investment Tracker</h1>
+          {signInSuccess && <Alert variant="success">{signInSuccess}</Alert>}
 
-    <Card style={{ width: '30rem' }}> 
-    <h2>Sign in to Investment Tracker</h2>
-    <h3>{signInSuccess}</h3>
-      <Card.Body>
-      <form onSubmit={doSignIn}>     
-        <input
-          type='text'
-          name='signinEmail'
-          placeholder='email' />
-        <input
-          type='password'
-          name='signinPassword'
-          placeholder='Password' />
-        {/* <button type='submit' variant="success">Sign in</button> */}
-        <Button type='submit' variant="success">Sign in</Button>
-      </form>
-      </Card.Body>
-    </Card> 
+          <Form onSubmit={doSignIn} className="mb-5">
+            <Form.Group controlId="formBasicEmail">
+              <Form.Label>Email address</Form.Label>
+              <Form.Control type="email" placeholder="Enter email" name='email' />
+            </Form.Group>
 
-    <Card style={{ width: '30rem' }}> 
-    <h2>New to Investment Tracker?</h2>   
-      <Card.Link href="/sign-up">Create an account</Card.Link>
-    </Card> 
+            <Form.Group className="mb-3" controlId="formBasicPassword">
+              <Form.Label>Password</Form.Label>
+              <Form.Control type="password" placeholder="Password" name='signinPassword' />
+            </Form.Group>
+            <Button variant="primary" type="submit">
+              Sign in
+            </Button>
+          </Form>
 
-      {/* <Card>
-      <h1>Sign Up</h1>
-      {signUpSuccess}
-      <form onSubmit={doSignUp}>
-        <input
-          type='text'
-          name='email'
-          placeholder='email' />
-        <input
-          type='password'
-          name='password'
-          placeholder='Password' />
-        <button type='submit'>Sign up</button>
-      </form>
-      </Card>
+          <h1 className="mb-3">New to Investment Tracker?</h1>
+          <a href="/sign-up" class="card-link">Sign Up</a>
 
-      <h1>Sign Out</h1>
-      {signOutSuccess}
-      <br />
-      <button onClick={doSignOut}>Sign out</button> */}
-    
+        </Col>
+      </Row>
+    </Container>
+
     </React.Fragment>
   );
 }
