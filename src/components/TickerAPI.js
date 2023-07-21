24d538917a1067bd602a87c7217bb0edde9ebcf6
nancyliqn91 <nancyliqn91@gmail.com>
2023-07-21 14:21:38 -0700

    WIP to add chart

--- src/components/TickerAPI.js
+++ src/components/TickerAPI.js
@@ -1,5 +1,15 @@
 import React, { useEffect, useState } from "react";
-import { Line } from 'react-chartjs-2';
+import {
+  Chart as ChartJS,
+  CategoryScale,
+  LinearScale,
+  BarElement,
+  Title,
+  Tooltip,
+  Legend,
+} from 'chart.js';
+import { Bar } from 'react-chartjs-2';
+import faker from 'faker';
 
 const apiKey = process.env.REACT_APP_API_KEY;
 const baseURL = "https://api.polygon.io/v2/aggs/ticker";
@@ -16,7 +26,6 @@ const TickerAPI = (props) => {
         const url = `${baseURL}/${props.name}/range/${props.multiplier}/${props.timespan}/${props.from}/${props.to}?adjusted=true&sort=asc&limit=20&apiKey=${apiKey}`;
 
         const response = await fetch(url);
-
         if (!response.ok) {
           throw new Error("Request failed with status code " + response.status);
         }
@@ -24,7 +33,6 @@ const TickerAPI = (props) => {
         const responseData = await response.json();
         setData(responseData.results);
         setChartData(generateChartData(responseData.results));
-
       } catch (error) {
         console.error(error);
         setError("Error generating ticker");
@@ -67,13 +75,18 @@ const TickerAPI = (props) => {
   return (
     <React.Fragment>
       <h1>Ticker Aggregates (Bars): {props.name} </h1>
-      <p>Explanation: Ticker is the exchange symbol that this item is traded under.</p>
+      <ul>
+        <li>Explanation: Ticker is the exchange symbol that this item is traded under.</li>
+        <li>Get aggregate bars for a stock over a given date range in custom time sizes.</li>
+      </ul>
+
       <ul>
         {data.map((stock, index) => (
           <li key={index}>
             <p>close price: {stock.c}</p>
             <p>highest price: {stock.h}</p>
             <p>lowest price: {stock.l}</p>
+            <p>number of transactions: {stock.n}</p>
             <p>open price: {stock.o}</p>
             <p>trading volume : {stock.v}</p>
             <p>volume weighted average price: {stock.vw}</p>           
@@ -81,11 +94,8 @@ const TickerAPI = (props) => {
         ))}
       </ul>
 
-      <h2>Line Chart</h2>
-      {/* <Line data={chartData} onElementsClick={handleChartClick} ref={(ref) => setChartInstance(ref)} /> */}
-      <Line data={chartData} />
-      {chartData && <Line data={chartData} />}
-
+      <h2>Bar Chart</h2>
+      {chartData && <Bar data={chartData} options={barChartOptions} ref={(ref) => setChartInstance(ref)} />}
     </React.Fragment>
   );
 };
