8530bcbe3e95640210ad6f711af4efe24775c77c
nancyliqn91 <nancyliqn91@gmail.com>
2023-07-21 15:16:49 -0700

    update header

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,28 +1,44 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import { auth } from "./../firebase.js";
+import { onAuthStateChanged } from "firebase/auth";
 import Container from 'react-bootstrap/Container';
 import Nav from 'react-bootstrap/Nav';
 import Navbar from 'react-bootstrap/Navbar';
+import SignOutButton from "./SignOutButton";
+import Logo from './../img/logo.jpg'
 
 function Header(){
+  const [isSignedIn, setIsSignedIn] = useState(null);
+  useEffect(() => {
+    const unsubscribe = onAuthStateChanged(auth, (user) => {
+      setIsSignedIn(!!user);
+    });
+    return () => unsubscribe();
+  }, []);
+
   return (
-    <React.Fragment>
-
-    {/* <h1 className="header">Welcome to Investment Tracker!</h1>
-    <h3 className="header">🌰 You are welcome to search and track your stocks and coins! 🌰</h3> */}
-
-      <Navbar bg="light" data-bs-theme="light">
+    <React.Fragment>   
+      <Navbar bg="dark" data-bs-theme="dark" fixed="top">
         <Container>
+        <img style={{width: 50, height: 50}} src={Logo} alt="a dinasour logo"/>
           <Navbar.Brand href="/">Investment Tracker</Navbar.Brand>
           <Nav className="me-auto">
-            <Nav.Link href="/">Manage Ticker</Nav.Link>
-            <Nav.Link href="/stocks">Stocks</Nav.Link>
-            <Nav.Link href="/sign-in">Sign In</Nav.Link>
-            <Nav.Link href="/sign-up">Sign Up</Nav.Link>
-            <Nav.Link href="/sign-out">Sign Out</Nav.Link>
+            <Nav.Link href="/">Manage Tickers</Nav.Link>
+            <Nav.Link href="/news">Stocks News</Nav.Link>
+
+            {!isSignedIn ? (
+            <>
+              <Nav.Link href="/sign-in">Sign In</Nav.Link>
+            </>
+            ) : (
+              <SignOutButton />
+            )}
+            
+            <Nav.Link href="/sign-up">Sign Up</Nav.Link> 
+            <Nav.Link href="/search">Search</Nav.Link>           
           </Nav>
         </Container>
       </Navbar>
-
     </React.Fragment>
   );
 }
