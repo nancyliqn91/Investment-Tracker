83de632bcb9c1dc7fc233a2ed3032ea224619b83
nancyliqn91 <nancyliqn91@gmail.com>
2023-07-25 14:24:11 -0700

    WIP

--- src/components/TickerAPI.js
+++ src/components/TickerAPI.js
@@ -1,16 +1,44 @@
 import React, { useEffect, useState } from "react";
-import { Chart as ChartJS, BarElement} from 'chart.js';
+import { Chart as ChartJS, BarElement, CategoryScale, LinearScale} from 'chart.js';
 import { Bar} from 'react-chartjs-2';
+import { format } from 'date-fns';
 
-ChartJS.register(LinearScale, CategoryScale, BarController, BarElement, TimeScale);
+ChartJS.register(LinearScale, CategoryScale, BarElement);
 
-const BarChart =() => {
+const TickerAPI =(props) => {
+  const apiKey = process.env.REACT_APP_API_KEY;
+  const baseURL = "https://api.polygon.io/v2/aggs/ticker";
+  // const [chartData, setChartData] = useState(null);
+  const [error, setError] = useState(null);
+  const [chart, setChart] = useState(null);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const url = `${baseURL}/${props.name}/range/${props.multiplier}/${props.timespan}/${props.from}/${props.to}?adjusted=true&sort=asc&limit=20&apiKey=${apiKey}`;
+
+        const response = await fetch(url);
+        if (!response.ok) {
+          throw new Error("Request failed with status code " + response.status);
+        }
+
+        const responseData = await response.json();
+        setChart(responseData);
+        // setChartData(generateChartData(responseData.results));
+      } catch (error) {
+        console.error(error);
+        setError("Error generating ticker");
+      }
+    };
+    fetchData();
+  }, [props]);
+
 
   const data = {
-    labels: ["red","blue","green","yellow","purple", "orange"],
+    labels: chart.results.map((stock) => format(new Date(stock.t), 'MM/dd/yyyy')),
     datasets: [{
-      label: '# of votes',
-      data: [65, 59, 80, 81, 56, 55, 40],
+      label: '{chart.results.count} Aggregates Available',
+      data: chart.results.map((stock) => (stock.c)),
       backgroundColor: [
         'rgba(255, 99, 132, 0.2)',
         'rgba(255, 159, 64, 0.2)',
@@ -61,4 +89,4 @@ const BarChart =() => {
   );
 }
 
-export default BarChart;
+export default TickerAPI;
