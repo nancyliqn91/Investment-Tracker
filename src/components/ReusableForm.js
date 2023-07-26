f13d1d62d2f45a7f12c6e4998d3b4b16baf0c481
nancyliqn91 <nancyliqn91@gmail.com>
2023-07-26 11:08:14 -0700

    update form style

--- src/components/ReusableForm.js
+++ src/components/ReusableForm.js
@@ -1,75 +1,78 @@
-import React, { useState } from "react";
+import React from "react";
 import PropTypes from "prop-types";
-import DatePicker from "react-datepicker";
-import "react-datepicker/dist/react-datepicker.css";
+import Form from 'react-bootstrap/Form';
+import Button from 'react-bootstrap/Button';
+import Container from 'react-bootstrap/Container';
+import Row from 'react-bootstrap/Row';
+import Col from 'react-bootstrap/Col';
 
 function ReusableForm(props) {
-  const [multiplier, setMultiplier] = useState(1);
-  const [from, setFrom] = useState(null);
-  const [to, setTo] = useState(null);
-  const [timespan, setTimespan] = useState("minute");
-
-  const handleMultiplierChange = (event) => {
-    setMultiplier(parseInt(event.target.value));
-  };
-
-  const handleFromDateChange = (date) => {
-    setFrom(date);
-  };
-
-  const handleToDateChange = (date) => {
-    setTo(date);
-  };
-
-  const handleTimespanChange = (event) => {
-    setTimespan(event.target.value);
-  };
-
-  const handleSubmit = (event) => {
-    event.preventDefault();
-    // Call the formSubmissionHandler with the form data
-    props.formSubmissionHandler({
-      multiplier,
-      from: from ? from.toISOString().split("T")[0] : "",
-      to: to ? to.toISOString().split("T")[0] : "",
-      timespan,
-    });
-  };
-
   return (
     <React.Fragment>
-      <form onSubmit={handleSubmit}>
-        <input
-          type="number"
-          name="multiplier"
-          placeholder="multiplier"
-          value={multiplier}
-          onChange={handleMultiplierChange}
-        />
+      <Container className="p-3 mb-2 bg-light bg-gradient text-dark rounded-5">
+        <Form onSubmit={props.formSubmissionHandler}>
+          <Row className='mb-3'>
+            <Col sm>
+              <Form.Group>
+              <Form.Label>Type your Ticker -e.g. "AAPL"</Form.Label> 
+              <Form.Control                
+                type='text'
+                name='ticker'
+                // placeholders passed down from Edit form
+                placeholder={props.name} />
+              </Form.Group>
+            </Col>
+          </Row>
 
-        <DatePicker
-          selected={from}
-          onChange={handleFromDateChange}
-          placeholderText="From"
-        />
-        <DatePicker
-          selected={to}
-          onChange={handleToDateChange}
-          placeholderText="To"
-        />
-
-        <select value={timespan} onChange={handleTimespanChange}>
-          <option value="minute">Minute</option>
-          <option value="hour">Hour</option>
-          <option value="day">Day</option>
-          <option value="week">Week</option>
-          <option value="month">Month</option>
-          <option value="quarter">Quarter</option>
-          <option value="year">Year</option>
-        </select>
-
-        <button type="submit">{props.buttonText}</button>
-      </form>
+          <Row className='mb-3'>
+            <Col sm>
+              <Form.Group >
+              <Form.Label>Multiplier?</Form.Label>
+              <Form.Control 
+                // input
+                type='number'
+                name='multiplier'
+                placeholder={props.multiplier} />
+              </Form.Group>
+            </Col>
+            <Col sm>
+              <Form.Group>
+              <Form.Label>Date From?</Form.Label>
+              <Form.Control 
+                // input
+                type='date'
+                name='from'
+                placeholder={props.from}/>
+              </Form.Group>
+            </Col>
+            <Col sm>
+              <Form.Group>
+              <Form.Label>Date To?</Form.Label>
+              <Form.Control 
+                // input
+                type='date'
+                name='to'
+                placeholder={props.to}/>
+              </Form.Group>
+            </Col>
+            <Col sm>
+              <Form.Group>
+              <Form.Label>Timespan?</Form.Label>
+              <select name="timespan">
+                <option value="minute">Minute</option>
+                <option value="hour">Hour</option>
+                <option value="day">Day</option>
+                <option value="week">Week</option>
+                <option value="month">Month</option>
+                <option value="quarter">Quarter</option>
+                <option value="year">Year</option>
+              </select>
+              </Form.Group>
+            </Col>
+          </Row>
+          <Button variant='primary' size='sm' type='submit' className="btn btn-primary">{props.buttonText}</Button>
+        </Form>
+      </Container>
     </React.Fragment>
   );
 }
@@ -81,44 +84,3 @@ ReusableForm.propTypes = {
 
 export default ReusableForm;
 
-
-// import React from "react";
-// import PropTypes from "prop-types";
-
-// function ReusableForm(props) {
-//   return (
-//     <React.Fragment>
-//       <form onSubmit={props.formSubmissionHandler}>
-//         <input
-//           type='text'
-//           name='name'
-//           placeholder='Ticker - "AAPL"' />
-//         <input
-//           type='text'
-//           name='multiplier'
-//           placeholder='multiplier -"5"' />
-//         <input
-//           type='text'
-//           name='timespan'
-//           placeholder='timespan - "minute"' />
-//         <input
-//           type='text'
-//           name='from'
-//           placeholder='from - "2023-01-09"' />
-//         <input
-//           type='text'
-//           name='to'
-//           placeholder='to - "2023-07-09"' />
-
-//         <button type='submit'>{props.buttonText}</button>
-//       </form>
-//     </React.Fragment>
-//   );
-// }
-
-// ReusableForm.propTypes = {
-//   formSubmissionHandler: PropTypes.func,
-//   buttonText: PropTypes.string
-// };
-
-// export default ReusableForm;
