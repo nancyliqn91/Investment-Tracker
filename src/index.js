514414377429c6ddbfb62d8c19423d4744045f25
nancyliqn91 <nancyliqn91@gmail.com>
2023-07-26 22:29:08 -0700

    WIP to host

--- src/index.js
+++ src/index.js
@@ -1,36 +1,23 @@
 import React from 'react';
-import ReactDOM from 'react-dom/client';
+// import ReactDOM from 'react-dom/client';
+import ReactDOM from 'react-dom';
 import './index.css';
 import App from './components/App';
 import reportWebVitals from './reportWebVitals';
+import { HashRouter } from 'react-router-dom';
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
+ReactDOM.render(
+  <HashRouter>
     <App />
-  </React.StrictMode>
+  </HashRouter>,
+  document.getElementById('root')
 );
-
-reportWebVitals();
-
-
-// import { createStore } from 'redux';
-// import { Provider } from 'react-redux';
-// import rootReducer from './reducers/index';
-
-// const store = createStore(rootReducer);
-// store.subscribe(() =>
-//   console.log(store.getState())
-// );
 
 // const root = ReactDOM.createRoot(document.getElementById('root'));
 // root.render(
 //   <React.StrictMode>
-
-//     <Provider store={store}>
 //     <App />
-//     </Provider>
 //   </React.StrictMode>
 // );
 
-// reportWebVitals();
+reportWebVitals();
